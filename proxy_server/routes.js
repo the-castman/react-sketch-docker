f2ac4da956fc7712f35a7450a8068030cef92b4e
Jose Castaneda <castman@joses-mac-mini.mynet>
2020-04-29 19:23:04 +0200

    Add field

--- proxy_server/routes.js
+++ proxy_server/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/sayhello', (req, res) => {
-    return res.json({ foo: 'bar' });
+    return res.json({ foo: 'bar', env: process.env.NODE_ENV });
 });
 
 module.exports = router;
